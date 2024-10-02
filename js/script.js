626e27b78024782b22dbba98349b67bca263c19b
Henrique de Andrade <108901290+riqueandrade@users.noreply.github.com>
2024-10-02 19:40:13 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
+//Em desenvolvimento
+
 const projetos = [
-    // ... (seu código existente para projetos) ...
+    //...
 ];
 
 function carregarProjetos(lang) {
-    // ... (seu código existente para carregar projetos) ...
+    //...
 }
 
 // Expor carregarProjetos globalmente
